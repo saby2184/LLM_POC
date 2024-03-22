ad2445b8f81cfcca8d773f52b1627ffd86f06785
Saby <saby2184@gmail.com>
2024-03-22 17:44:53 +0530

    Added retrieval from the local chromaDB

--- llm_doc_read/app.py
+++ llm_doc_read/app.py
@@ -2,7 +2,10 @@ import streamlit as st
 from langchain.prompts import PromptTemplate
 #from langchain_community.llms import CTransformers
 from langchain_community.llms import LlamaCpp
-
+from langchain.chains import RetrievalQA
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceInstructEmbeddings
+import torch
 
 ##call doc
 ##def get_resp(doc,query):
@@ -31,14 +34,18 @@ Answer: Explain the answer briefly."""
 prompt = PromptTemplate.from_template(template)
 
 
-
-
+db_dir="./chroma_db"
+DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 n_gpu_layers = 1  # Metal set to 1 is enough.
 n_batch = 512  # Should be between 1 and n_ctx, consider the amount of RAM of your Apple Silicon Chip.
 
 
 
-
+#Vector store
+embeddings = HuggingFaceInstructEmbeddings(
+        model_name="hkunlp/instructor-large", model_kwargs={"device": DEVICE}
+    )
+db = Chroma(persist_directory=db_dir, embedding_function=embeddings)
 
 
 
@@ -55,7 +62,20 @@ llm = LlamaCpp(
 )
 #print(llm.invoke("Simulate a rap battle between Stephen Colbert and John Oliver") )
 
-prompt = """
-Question: Explain what is an transformers in large language models
+
+retriever = db.as_retriever()
+qa = RetrievalQA.from_chain_type(
+    llm=llm, 
+    #chain_type_kwargs={"prompt": prompt}, 
+    retriever=retriever, 
+    verbose=True
+)
+
+
+query = """
+Question: what is multihead attention
 """
-print(llm.invoke(prompt))
+#print(llm.invoke(prompt))
+
+result = qa({"query": query})
+print(result)
